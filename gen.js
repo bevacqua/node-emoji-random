1858de16440989799a2d70145997401d703e5108
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-11 21:02:43 -0300

    :+1: looks awesome

---
+++ gen.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var fs = require('fs');
+var async = require('async');
+var sp = require('child_process').spawn;
+var emoji = require('./emoji.json');
+
+async.eachSeries(emoji.names, function(name, next){
+
+    fs.writeFileSync(__dirname + 'all' + name.replace(/:/, ''));
+    var git = sp('git', ['add', '.']);
+
+    git.on('close', function(){
+        git = sp('git', ['commit', '-am', name + ' looks awesome']);
+        // git.on('close', next);
+        console.log('then..');
+    });
+});
